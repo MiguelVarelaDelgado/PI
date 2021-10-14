3ff3dec1e9e738fcf3de988fd88112b049df5a52
MiguelVarelaDelgado <1160250@alumno.um.edu.mx>
2021-10-14 08:03:37 -0500

    Entrega 14 de Octubre

--- PI3.py
+++ PI3.py
@@ -17,15 +17,17 @@ from sklearn import svm
 from sklearn.neural_network import MLPClassifier
 def LeerCSV(adressTrain):
     W_P=pd.read_csv(adressTrain)
-    X_prime = W_P.iloc[:,[1,2,3,4,5,6]].values
-    Y = W_P.iloc[:,0].values
+    X_prime = W_P.iloc[:,[0,1,2]].values
+    Y = W_P.iloc[:,3].values
+    X_prime
+    
     return (X_prime, Y)
 def CrearModelos(X_train,X_test,y_train,y_test):    
     ##      KNN
-    ModeloKNN=neighbors.KNeighborsClassifier(n_neighbors=10)
+    ModeloKNN=neighbors.KNeighborsClassifier()
     ModeloKNN.fit(X_train, y_train)
     ##      Decision tree
-    ModelosTree = tree.DecisionTreeClassifier(max_leaf_nodes=20)
+    ModelosTree = tree.DecisionTreeClassifier()
     ModelosTree.fit(X_train, y_train)
     tree.plot_tree(ModelosTree)
     ##      Logistic Regression
@@ -35,6 +37,7 @@ def CrearModelos(X_train,X_test,y_train,y_test):
     ModeloSVM = svm.SVC()
     ModeloSVM.fit(X_train, y_train)
     ##      Artificial Neural Network
+    
     modelRed = MLPClassifier(hidden_layer_sizes=(7,), max_iter=150,activation = 'relu',solver='adam',random_state=1)
     modelRed.fit(X_train, y_train)
 
@@ -53,22 +56,22 @@ def CrearPredicciones(ModeloKNN, ModelosTree, ModeloLR, ModeloSVM,modelRed):
     y_predRed   =modelRed.predict(X_test)
     ##      KNN
     ##print("accuracy del modelo KNN\n",metrics.accuracy_score(y_test, y_predKNN))
-    print("Reporte del modelo KNN\n",metrics.classification_report(y_test, y_predKNN))
+    print("accuracy del modelo KNN\n",metrics.classification_report(y_test, y_predKNN))
     ##      Decision tree}
     ##print("accuracy del modelo Decision tree\n",metrics.accuracy_score(y_test, y_predTree))
-    print("Reporte del modelo Decision tree\n",metrics.classification_report(y_test, y_predTree))
+    print("accuracy del modelo Decision tree\n",metrics.classification_report(y_test, y_predTree))
 
     ##      Logistic Regression
     ##print("accuracy del modelo Logistic Regression\n",metrics.accuracy_score(y_test, y_predLR))
-    print("Reporte del modelo Logistic Regression\n",metrics.classification_report(y_test, y_predLR))
+    print("accuracy del modelo Logistic Regression\n",metrics.classification_report(y_test, y_predLR))
 
     ##      Support Vector Machines
     ##print("accuracy del modelo Support Vector Machines\n",metrics.accuracy_score(y_test, y_predSVM))
-    print("Reporte del modelo Support Vector Machines\n",metrics.classification_report(y_test, y_predSVM))
+    print("accuracy del modelo Support Vector Machines\n",metrics.classification_report(y_test, y_predSVM))
     
     ##      Artificial Neural Network
     ##print("accuracy del modelo red\n",metrics.accuracy_score(y_test, y_predRed))
-    print("Reporte del modelo Artificial Neural Network\n",metrics.classification_report(y_test, y_predRed))
+    print("accuracy del modelo Artificial Neural Network\n",metrics.classification_report(y_test, y_predRed))
     
     #y_pred   =modelRed.predict(X_train)
     #print(metrics.classification_report(y_train, y_pred))
@@ -82,7 +85,7 @@ def Normalizar(X_prime):
 
 
 
-(X_prime, Y) = LeerCSV('Admission2.csv')
+(X_prime, Y) = LeerCSV('NEW2.csv')
 
 (X,scaler)=Normalizar(X_prime)
 
